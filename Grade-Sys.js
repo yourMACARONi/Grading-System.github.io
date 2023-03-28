170786bff49be959cc195b7642d473544817825b
yourMACARONi <126305477+yourMACARONi@users.noreply.github.com>
2023-03-29 03:19:35 +0800

    ballin

---
+++ Grade-Sys.js
@@ -0,0 +1,37 @@
+const getResult = () => {
+
+    let wsys = document.getElementById('wsys').value;
+    let sia = document.getElementById('sia').value;
+    let im = document.getElementById('im').value;
+    let networking = document.getElementById('networking').value;
+    let ethics = document.getElementById('ethics').value;
+
+    let total = parseFloat(wsys) + parseFloat(sia) + parseFloat(im) + parseFloat(networking) + parseFloat(ethics);
+    let percent=(total*100)/500;
+
+        if(percent>=95){
+            document.getElementById('grade').innerHTML = "Very Satisfactory";
+        }
+        else if(percent>=90){
+            document.getElementById('grade').innerHTML = "Ahhh Satisfactory";
+        }
+        else if(percent>=85){
+            document.getElementById('grade').innerHTML = "Very Good";
+        }
+        else if(percent>=80){
+            document.getElementById('grade').innerHTML = "Good";
+        }
+        else if(percent>=75){
+            document.getElementById('grade').innerHTML = "Fair";
+        }
+        else if(percent>=61){
+            document.getElementById('grade').innerHTML = "FAILED";
+        }
+        else{
+            document.getElementById('grade').innerHTML = "RETAKE";
+        }
+
+
+    document.getElementById('total').innerHTML = total;
+    document.getElementById('percent').innerHTML = percent;
+}
